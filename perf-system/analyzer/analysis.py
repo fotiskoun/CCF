6691fd0211ed336bcb37ab7378d5751a6a5d9143
Fotis Kounelis <fotisss17@gmail.com>
2022-10-24 17:34:37 +0100

    make tables and basic search functions in analysis as functions to work towards python lib

--- perf-system/analyzer/analysis.py
+++ perf-system/analyzer/analysis.py
@@ -11,47 +11,64 @@ import pandas as pd  # type: ignore
 from prettytable import PrettyTable  # type: ignore
 import numpy as np
 import matplotlib.pyplot as plt  # type: ignore
+from typing import List
+
+latency_list = []
+ms_latency_list = []
+SEC_MS = 1000
 
 
-def make_analysis(send_file, response_file):
-    """
-    Produce the analysis results
-    """
-    df_sends = pd.read_parquet(send_file, engine="fastparquet")
-    df_responses = pd.read_parquet(response_file, engine="fastparquet")
+def get_latency_list() -> list:
+    return latency_list
 
+
+def get_ms_latency_list() -> list:
+    return ms_latency_list
+
+
+def get_req_type(df_responses: pd.DataFrame) -> str:
+    return df_responses.iloc[0]["rawResponse"].split(" ")[0]
+
+
+def get_latency_at_i(df_sends, df_responses, req_id):
+    # will need to handle the re-submission response from submitter when decided
+    return df_responses.iloc[req_id]["receiveTime"] - df_sends.iloc[req_id]["sendTime"]
+
+
+def check_success(df_responses, req_id) -> int:
+    req_resp = df_responses.iloc[req_id]["rawResponse"].split("\n")
+    status_list = req_resp[0].split(" ")
+    # if we get a full statues and says ok increase the successful
+    if len(status_list) > 1 and status_list[1][:3] == "200":
+        return 1
+    return 0
+
+
+def iter_for_success_and_latency(df_sends, df_responses) -> float:
     successful_reqs = 0
-    time_spent_list = []
-    ms_time_spent_sum = 0
 
     for i in range(len(df_sends.index)):
-        # the number of rows in both dfs is the same
-        assert (
-            df_sends.iloc[i]["messageID"] == df_responses.iloc[i]["messageID"]
-        ), "the IDs do not match"
+        successful_reqs += check_success(df_responses, i)
 
-        req_resp = df_responses.iloc[i]["rawResponse"].split("\n")
-        status_list = req_resp[0].split(" ")
-        req_type_received = status_list[0]
-        # if we get a full statues and says ok increase the successful
-        if len(status_list) > 2 and (
-            status_list[2][:2] == "OK" or status_list[1][:3] == "200"
-        ):
-            successful_reqs += 1
+        latency_i = get_latency_at_i(df_sends, df_responses, i)
+        latency_list.append(latency_i)
+        ms_latency_list.append(latency_i * SEC_MS)
 
-        time_spent_list.append(
-            df_responses.iloc[i]["receiveTime"] - df_sends.iloc[i]["sendTime"]
-        )
+    return successful_reqs / len(df_sends.index) * 100
 
-    successful_percent = successful_reqs / len(df_sends.index) * 100
 
-    # time_spent is always: last timestamp of responses - first timestamp of sends
-    time_spent = df_responses.iloc[-1]["receiveTime"] - df_sends.iloc[0]["sendTime"]
+def total_time_in_sec(df_sends, df_responses):
+    # time_spent is: last timestamp of responses - first timestamp of sends
+    return df_responses.iloc[-1]["receiveTime"] - df_sends.iloc[0]["sendTime"]
 
-    ms_time_spent_list = [x * 1000 for x in time_spent_list]
-    ms_time_spent_sum = time_spent * 1000
 
+def sec_to_ms(time_in_sec):
+    return time_in_sec / SEC_MS
+
+
+def time_success_thoughput_table(df_sends, df_responses, successful_percent):
     generic_output_table = PrettyTable()
+
     generic_output_table.field_names = [
         "Total Requests",
         "Total Time (s)",
@@ -59,6 +76,10 @@ def make_analysis(send_file, response_file):
         "Fail (%)",
         "Throughput (req/s)",
     ]
+
+    time_spent = total_time_in_sec(df_sends, df_responses)
+    ms_time_spent_sum = sec_to_ms(time_spent)
+
     generic_output_table.add_row(
         [
             len(df_sends.index),
@@ -68,6 +89,11 @@ def make_analysis(send_file, response_file):
             round(len(df_sends.index) / time_spent, 1),
         ]
     )
+    return generic_output_table
+
+
+def latencies_table(df_sends, df_responses):
+    ms_time_spent_sum = total_time_in_sec(df_sends, df_responses) * SEC_MS
     latency_output_table = PrettyTable()
     latency_output_table.field_names = [
         "Latency (ms)",
@@ -80,20 +106,39 @@ def make_analysis(send_file, response_file):
     ]
     latency_output_table.add_row(
         [
-            round(np.percentile(ms_time_spent_list, 50), 3),
+            round(np.percentile(ms_latency_list, 50), 3),
             round(ms_time_spent_sum / len(df_sends.index), 3),
-            round(np.percentile(ms_time_spent_list, 80), 3),
-            round(np.percentile(ms_time_spent_list, 90), 3),
-            round(np.percentile(ms_time_spent_list, 95), 3),
-            round(np.percentile(ms_time_spent_list, 99), 3),
-            round(np.percentile(ms_time_spent_list, 99.9), 3),
+            round(np.percentile(ms_latency_list, 80), 3),
+            round(np.percentile(ms_latency_list, 90), 3),
+            round(np.percentile(ms_latency_list, 95), 3),
+            round(np.percentile(ms_latency_list, 99), 3),
+            round(np.percentile(ms_latency_list, 99.9), 3),
         ]
     )
+    return latency_output_table
 
-    print("The request type sent is ", req_type_received)
 
-    print(generic_output_table)
-    print(latency_output_table)
+def customize_table(fields_list, values_list: List[List]):
+    custom_table = PrettyTable()
+    custom_table.field_names = fields_list
+    for val_row in values_list:
+        custom_table.add_row(val_row)
+    return custom_table
+
+
+def make_analysis(send_file, response_file):
+    """
+    Produce the analysis results
+    """
+    df_sends = pd.read_parquet(send_file, engine="fastparquet")
+    df_responses = pd.read_parquet(response_file, engine="fastparquet")
+
+    successful_percent = iter_for_success_and_latency(df_sends, df_responses)
+
+    print("The request type sent is ", get_req_type(df_responses))
+
+    print(time_success_thoughput_table(df_sends, df_responses, successful_percent))
+    print(latencies_table(df_sends, df_responses))
 
     x = ["-"] * 20
     print("\n", "".join(x), " Start plotting  ", "".join(x))
@@ -102,7 +147,7 @@ def make_analysis(send_file, response_file):
     ]
 
     id_unit = [x for x in range(0, len(df_sends.index))]
-    lat_unit = ms_time_spent_list
+    lat_unit = ms_latency_list
 
     # sort the latencies as it make sense to get the throughput
     # by time unit ignoring the ids
@@ -136,7 +181,7 @@ def make_analysis(send_file, response_file):
     plt.figure(figsize=(15, 15), dpi=80)
     # plot latency with time
     plt.figure(2)
-    plt.scatter(time_unit, ms_time_spent_list, s=1)
+    plt.scatter(time_unit, ms_latency_list, s=1)
     plt.ylabel("Latency(ms)")
     plt.xlabel("time(s)")
     plt.savefig("latency_across_time.png")
