7585ee1795db52488305929cd57b77dcd1479e52
Fotis Kounelis <fotisss17@gmail.com>
2022-11-11 10:25:47 +0000

    add more applications

--- tests/perf-system/concurrent_run.sh
+++ tests/perf-system/concurrent_run.sh
@@ -9,4 +9,16 @@ cd "$BUILD_DIR"
 P1=$!
 ./submit -c ./workspace/sandbox_common/user0_cert.pem -k ./workspace/sandbox_common/user0_privk.pem --cacert ./workspace/sandbox_common/service_cert.pem --generator-filepath ../tests/perf-system/generator/new_100k_raw.parquet --response-filepath "../tests/perf-system/submitter/cpp_respond2.parquet" --send-filepath "../tests/perf-system/submitter/cpp_send2.parquet" -m 1000 &
 P2=$!
-wait $P1 $P2
+./submit -c ./workspace/sandbox_common/user0_cert.pem -k ./workspace/sandbox_common/user0_privk.pem --cacert ./workspace/sandbox_common/service_cert.pem --generator-filepath ../tests/perf-system/generator/new_100k_raw.parquet --response-filepath "../tests/perf-system/submitter/cpp_respond3.parquet" --send-filepath "../tests/perf-system/submitter/cpp_send3.parquet" -m 1000 &
+P3=$!
+./submit -c ./workspace/sandbox_common/user0_cert.pem -k ./workspace/sandbox_common/user0_privk.pem --cacert ./workspace/sandbox_common/service_cert.pem --generator-filepath ../tests/perf-system/generator/new_100k_raw.parquet --response-filepath "../tests/perf-system/submitter/cpp_respond4.parquet" --send-filepath "../tests/perf-system/submitter/cpp_send4.parquet" -m 1000 &
+P4=$!
+./submit -c ./workspace/sandbox_common/user0_cert.pem -k ./workspace/sandbox_common/user0_privk.pem --cacert ./workspace/sandbox_common/service_cert.pem --generator-filepath ../tests/perf-system/generator/new_100k_raw.parquet --response-filepath "../tests/perf-system/submitter/cpp_respond5.parquet" --send-filepath "../tests/perf-system/submitter/cpp_send5.parquet" -m 1000 &
+P5=$!
+./submit -c ./workspace/sandbox_common/user0_cert.pem -k ./workspace/sandbox_common/user0_privk.pem --cacert ./workspace/sandbox_common/service_cert.pem --generator-filepath ../tests/perf-system/generator/new_100k_raw.parquet --response-filepath "../tests/perf-system/submitter/cpp_respond6.parquet" --send-filepath "../tests/perf-system/submitter/cpp_send6.parquet" -m 1000 &
+P6=$!
+./submit -c ./workspace/sandbox_common/user0_cert.pem -k ./workspace/sandbox_common/user0_privk.pem --cacert ./workspace/sandbox_common/service_cert.pem --generator-filepath ../tests/perf-system/generator/new_100k_raw.parquet --response-filepath "../tests/perf-system/submitter/cpp_respond7.parquet" --send-filepath "../tests/perf-system/submitter/cpp_send7.parquet" -m 1000 &
+P7=$!
+./submit -c ./workspace/sandbox_common/user0_cert.pem -k ./workspace/sandbox_common/user0_privk.pem --cacert ./workspace/sandbox_common/service_cert.pem --generator-filepath ../tests/perf-system/generator/new_100k_raw.parquet --response-filepath "../tests/perf-system/submitter/cpp_respond.parquet" --send-filepath "../tests/perf-system/submitter/cpp_send8.parquet" -m 1000 &
+P8=$!
+wait $P1 $P2 $P3 $P4 $P5 $P6 $P7 $P8
